8961fe1f47680a88e756593096c0dcb5f05dc0c1
Ritesh Kumar <riteshkumar477823@gmail.com>
2026-08-03 15:05:42 +0530

    Simplify llama-server execution in start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-cd /app/llama.cpp
-
-exec ./build/bin/llama-server \
+exec llama-server \
   --hf-repo HuggingFaceTB/SmolLM2-360M-Instruct-GGUF \
   --hf-file smollm2-360m-instruct-q8_0.gguf \
   --host 0.0.0.0 \
