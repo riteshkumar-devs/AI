f9f9f96627298a484c124732e36daae4dfbc21db
Ritesh Kumar <riteshkumar477823@gmail.com>
2026-08-03 16:15:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 exec ./llama-server \
-  --hf-repo HuggingFaceTB/SmolLM2-360M-Instruct-GGUF \
-  --hf-file smollm2-360m-instruct-q4_k_m.gguf \
+  --hf-repo bartowski/SmolLM2-360M-Instruct-GGUF \
+  --hf-file SmolLM2-360M-Instruct-Q4_K_M.gguf \
   --host 0.0.0.0 \
   --port 10000 \
   -c 2048 \
