c34af1d72a4c182aeb9017832638eec589d59641
Ritesh Kumar <riteshkumar477823@gmail.com>
2026-08-03 14:32:38 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-bash /app/download-model.sh
-
 cd /app/llama.cpp
 
 ./build/bin/llama-server \
-  -m models/SmolLM2-360M-Instruct-Q4_K_M.gguf \
+  -hf tensorblock/SmolLM2-360M-Instruct-GGUF:Q4_K_M \
   --host 0.0.0.0 \
   --port 10000 \
   -c 512
